b16f6dfc96dd01e349d3dad9dab0b17572608d75
lue-bird <lue.the.bird@gmail.com>
2026-02-16 22:54:00 +0100

    condense vscode entrypoint code

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -1,50 +1,29 @@
 import * as vscode from "vscode";
-import {
-  LanguageClientOptions,
-} from "vscode-languageclient";
-import {
-  LanguageClient,
-  ServerOptions,
-} from "vscode-languageclient/node";
+import * as language_client_node from "vscode-languageclient/node";
 import * as child_process from "node:child_process";
 
-let client: LanguageClient | null = null;
-
+// switch to your locally built debug executable path when developing
+const languageServerExecutableName: string = "still";
+let client: language_client_node.LanguageClient | null = null;
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-  const languageServerExecutableName: string =
-    // switch to your locally built debug executable path when developing
-    "still";
-  context.subscriptions.push(vscode.commands.registerCommand("still.commands.restart", async () => {
-    if (client !== null) {
-      await client.stop();
-      await client.start();
-    }
-  }));
-
-  const serverOptions: ServerOptions = async () => {
-    return child_process.spawn(languageServerExecutableName)
-  };
-  const clientOptions: LanguageClientOptions = {
-    diagnosticCollectionName: "still",
-    documentSelector: [
-      {
-        scheme: "file",
-        language: "still",
-      }
-    ],
-    synchronize: {
-      fileEvents: vscode.workspace.createFileSystemWatcher("**/*.still")
+  client = new language_client_node.LanguageClient(
+    "still",
+    "still",
+    async () => child_process.spawn(languageServerExecutableName),
+    {
+      diagnosticCollectionName: "still",
+      documentSelector: [{ scheme: "file", language: "still" }],
+      synchronize: {
+        fileEvents: vscode.workspace.createFileSystemWatcher("**/*.still")
+      },
     },
-  };
-  client = new LanguageClient(
-    "still",
-    "still",
-    serverOptions,
-    clientOptions,
   );
+  context.subscriptions.push(vscode.commands.registerCommand("still.commands.restart", async () => {
+    await client?.stop();
+    await client?.start();
+  }));
   await client.start();
 }
-
 export function deactivate(): Thenable<void> | undefined {
   if (client !== null) {
     return client.stop()
